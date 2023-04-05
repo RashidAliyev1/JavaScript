d8cdb6240df51c67d1c125033fe87c8dd5ee9bb0
RashidAliyev1 <126706582+RashidAliyev1@users.noreply.github.com>
2023-04-05 03:04:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log(4 + 2 + "5" + 2 + "a");
+console.log(12 * 8);
+let x=10/2
+console.log(x);
+
+let num1=20, num2=17
+console.log(num1+num2);
+
+let name="Rashid",surname="Aliyev",year="2003"
+console.log(name+" "+ surname+" "+ year)
+
+let num=17%12
+console.log(x);
+
+let cityName="Baku"
+cityName="Ganja"
+console.log(cityName)
+
+let obj1 = {name2:"Rashid"
+}
+console.log(obj1)
+let obj2 = obj1
+console.log(obj2)
+
+
+
+
+
+
