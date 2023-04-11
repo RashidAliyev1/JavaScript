bbe1bc3af97feb14d5a03ecc0f9f338ef362256a
RashidAliyev1 <126706582+RashidAliyev1@users.noreply.github.com>
2023-04-11 13:48:33 +0400

    Add files via upload

--- JS script.js/main.js
+++ JS script.js/main.js
@@ -7,8 +7,51 @@ const obj2=obj1
 obj1.id=3;
 console.log(obj1)
 
+
+
+for(let value in obj1){
+    console.log(obj1[value])
+}
+
+
+for(let key in obj1){
+    console.log(key)
+}
+
 delete obj1.location
 console.log(obj1)
+
+const newObj={}
+const obj=Object.assign(newObj)
+console.log
+
+//Array
+const numbers=[0,1,2,3,4,5,6,7,8,9,10]
+console.log(numbers)
+
+const arr2=[1,true,"true"]
+console.log(arr2)
+
+const arr3=[0,1,2,3,4,5]
+const arr4=arr2;
+console.log(arr4)
+
+const arr5=[0,1,2,3,4,5,6,7]
+arr5.push(8,9)
+console.log(arr5)
+
+const arr6=[]
+arr6.push(5,6,7)
+arr6[0]=1
+console.log(arr6)
+
+const arr7=["Rashid","Aliyev",20,"Quba"]
+arr7[3]="Qusar"
+console.log(arr7)
+
+for(let num of numbers){
+    console.log(num)
+}
 
 
 
